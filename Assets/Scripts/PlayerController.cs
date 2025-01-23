94a5cc68d9357677d3f85b2d4e9eb68cb80a20c4
emiliapiesnikowska <emila.piesnikowska@gmail.com>
2025-01-23 12:58:47 +0100

    initial project version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+
+
+    private int desiredLane = 1;
+    public float laneDistance = 4; //odlegloœæ pomiedzy 2 liniami 
+
+    public float jumpForce;
+
+    public float Gravity = -20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+
+
+
+        direction.y += Gravity*Time.deltaTime;
+
+        if(controller.isGrounded){
+            if(Input.GetKeyDown(KeyCode.UpArrow)){
+                    Jump();
+            }
+
+        }
+        else {
+            direction.y += Gravity *Time.deltaTime;
+        }
+
+
+        
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            desiredLane++;
+            if(desiredLane==3)
+                desiredLane = 2;
+        }
+
+         if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            desiredLane--;
+            if(desiredLane==-1) 
+                desiredLane = 0;
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if(desiredLane == 0){
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLane == 2){
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition,60*Time.fixedDeltaTime);
+    }
+
+    private void FixedUpdate(){
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void Jump(){
+        direction.y = jumpForce;
+    }
+}
